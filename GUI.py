05a8cfbd3d3edc5983e204e293c084189f6491d4
RobinPetit <robpetit@ulb.ac.be>
2015-04-20 22:09:44 +0200

    added constant movement

--- GUI.py
+++ GUI.py
@@ -5,24 +5,30 @@ from math import cos, sin, pi
 from Snake import Snake
 
 class GUI:
+    DEFAULT_REFRESH_TIMER = 25
     def __init__(self):
         self.fenetre = Tk()
         self.fenetre.geometry('480x480')
         self.canvas = Canvas(self.fenetre, bg='black', highlightthickness=0)
         self.canvas.pack(fill='both', expand=1)
-
-        self.w = 8
+        self.timer = GUI.DEFAULT_REFRESH_TIMER
         self.snake = Snake(self.canvas, randint(0, 480),
                            randint(0, 480), random()*2*pi)
-        self.snake.move()
+        self.refresh()
         self.canvas.focus_set()
         self.canvas.bind('<Key>', self.keyPressed)
-
+    
+    def refresh(self):
+        self.snake.move()
+        self.current_loop = self.fenetre.after(self.timer, self.refresh)
+    
     def keyPressed(self, e):
         touche = e.keysym
         if touche in ('Right', 'Left'):
             self.snake.angle += 0.15 * {'Right': 1, 'Left': -1}[touche]
-        self.snake.move()
+        elif touche == 'q':
+            self.fenetre.after_cancel(self.current_loop)
+        # self.snake.move()
     
     def start(self):
         self.fenetre.mainloop()
--- Snake.py
+++ Snake.py
@@ -1,26 +1,27 @@
-DEFAULT_SPEED = 4
-DEFAULT_THICKNESS = 8
+DEFAULT_SPEED = 2
+DEFAULT_THICKNESS = 4
 
 from math import cos, sin, pi
 
 class Snake:
-    def __init__(self, canvas, x_head, y_head, angle, speed=DEFAULT_SPEED, thickness=DEFAULT_THICKNESS):
+    def __init__(self, canvas, x_head, y_head, angle, speed=DEFAULT_SPEED,
+                                                thickness=DEFAULT_THICKNESS):
         #list and not tuple because tuples can't be modified
         self.head_coord = [x_head, y_head]
         self.speed = speed
         self.angle = angle #radians!
         self.canvas = canvas
         self.thickness = thickness
-        self.head_id = self.canvas.create_rectangle(x_head - thickness,
-                                                    y_head - thickness,
-                                                    x_head + thickness,
-                                                    y_head + thickness,
-                                                    fill='white')
+        self.move()
+        # self.head_id = self.canvas.create_rectangle(
+                        # x_head - thickness, y_head - thickness,
+                        # x_head + thickness, y_head + thickness, fill='white')
     
     def move(self):
         x, y = self.head_coord
         x += self.speed * cos(self.angle)
         y += self.speed * sin(self.angle)
         self.head_coord = [x, y]
-        radius = self.thickness // 2
-        self.canvas.coords(self.head_id, x-radius, y-radius, x+radius, y+radius)
+        r = self.thickness // 2
+        self.canvas.create_rectangle(x-r, y-r, x+r, y+r, fill='white', outline='white')
+        # self.canvas.coords(self.head_id, x-r, y-r, x+r, y+r)
