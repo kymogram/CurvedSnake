9016a8914792b6149bfb773b75990c79c5b23e91
RobinPetit <robpetit@ulb.ac.be>
2015-04-22 09:49:32 +0200

    added collisions management

--- GUI.py
+++ GUI.py
@@ -1,7 +1,6 @@
 from tkinter import *
 from random import randint, random
 from math import cos, sin, pi
-from tkinter import Toplevel
 
 from Snake import Snake
 
@@ -11,33 +10,41 @@ class GUI:
         self.window = Tk()
         self.window.geometry('480x480')
         self.timer = GUI.DEFAULT_REFRESH_TIMER
+        self.current_loop = 0
         self.menuStart()
         self.window.mainloop()
     
     def refresh(self):
-        self.snake.move()
+        self.snake.move(self.current_loop)
         self.current_loop = self.window.after(self.timer, self.refresh)
     
     def keyPressed(self, e):
         touche = e.keysym
         if touche in ('Right', 'Left'):
-            self.snake.angle += 0.15 * {'Right': 1, 'Left': -1}[touche]
-        elif touche == 'q':
-            self.window.after_cancel(self.current_loop)
+            self.snake.angle += 0.15 * {'R': 1, 'L': -1}[touche[0]]
+        elif touche.lower() == 'q':
+            self.quitCurrentPlay()
         
+    def quitCurrentPlay(self):
+        self.window.after_cancel(self.current_loop)
+        self.menuStart()
+    
+    def clearWindow(self):
+        for child in self.window.winfo_children():
+            child.pack_forget()
+    
     def menuStart(self):
+        self.clearWindow()
         Label(self.window, width=100, text='Curved Snake').pack()
         playButton = Button(self.window, text='Play!', command=self.playPressed).pack()
     
     def playPressed(self):
-        for child in self.window.winfo_children():
-            child.pack_forget()
-        self.play()
+        self.clearWindow()
+        self.window.after(1000, self.play)
     
     def play(self):
         self.canvas = Canvas(self.window, bg='black', highlightthickness=0)
         self.canvas.pack(fill='both', expand=1)
-        
         self.snake = Snake(self.canvas, randint(0, 480),
                            randint(0, 480), random()*2*pi)
         self.refresh()
--- Snake.py
+++ Snake.py
@@ -4,21 +4,41 @@ DEFAULT_THICKNESS = 4
 from math import cos, sin, pi
 
 class Snake:
-    def __init__(self, canvas, x_head, y_head, angle, speed=DEFAULT_SPEED,
-                                                thickness=DEFAULT_THICKNESS):
+    def __init__(self, canvas, x_head, y_head, angle, color='white',
+                            speed=DEFAULT_SPEED, thickness=DEFAULT_THICKNESS):
         #list and not tuple because tuples can't be modified
         self.head_coord = [x_head, y_head]
         self.speed = speed
         self.angle = angle #radians!
         self.canvas = canvas
         self.thickness = thickness
-        self.move()
+        self.color = color
+        self.alive = True
     
-    def move(self):
+    def move(self, step):
+        #stop moving if snake is dead
+        if not self.alive:
+            return
+        #step is used to handle collisions
+        if not isinstance(step, int):
+            #if it is under form 'after#{}', remove 'after#'
+            step = int(step[step.find('#')+1:])
+        #find new coordinates
         x, y = self.head_coord
         x += self.speed * cos(self.angle)
         y += self.speed * sin(self.angle)
+        #update head_coord
         self.head_coord = [x, y]
+        #radius of oval
         r = self.thickness // 2
-        self.canvas.create_rectangle(x-r, y-r, x+r, y+r, fill='white', outline='white')
-
+        #find all items in contact with new position
+        collisions = self.canvas.find_overlapping(x-r, y-r, x+r, y+r)
+        #if there is contact, snake is dead
+        #WARNING: when the snake moves, it always touches its previous position
+        #so step of appearance is written as a tag.
+        if len(collisions) != 0 and int(self.canvas.gettags(int(collisions[0]))[0]) < step-self.thickness:
+            self.alive = False
+        self.canvas.create_rectangle(x-r, y-r, x+r, y+r,
+                                    fill=self.color, outline=self.color,
+                                    tag=str(step))
+    #
