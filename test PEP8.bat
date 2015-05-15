fcef285a5488b2f1d0bae3f4af28d4c673432963
RobinPetit <robpetit@ulb.ac.be>
2015-05-15 03:00:58 +0200

    comment for bat

--- test PEP8.bat
+++ test PEP8.bat
@@ -9,4 +9,5 @@ SET files=Core/Arc.py ^
           Core/RandomBonus.py ^
           Core/ComboColorBox.py
 SET output=flake8.txt
+REM writes result of PEP8 analysis in output file
 flake8 %files%>%output%
